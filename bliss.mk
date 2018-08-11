5cd50a3982b70717e51ea12d297a45a6c04ae009
Jon West <electrikjesus@gmail.com>
2018-08-11 17:04:28 -0400

    make sepolicy conditional

--- bliss.mk
+++ bliss.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
-$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+$(call inherit-product-if-exists, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/bliss/build/core/config.mk
 -include vendor/bliss/build/core/apicheck.mk
