16b625c70cbf14a1750bf9363327c74d8af83584
Jon West <electrikjesus@gmail.com>
2018-08-12 21:26:28 -0400

    Add vendorsetup
    Just in case someone wants to use lunch menu
    
    Change-Id: I7ad8ff103dfb0fa7423e2705bb344a4a8678486a

---
+++ vendorsetup.sh
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo treble_arm64_afN-userdebug
+add_lunch_combo treble_arm64_afS-userdebug
+add_lunch_combo treble_arm64_agN-userdebug
+add_lunch_combo treble_arm64_agS-userdebug
+add_lunch_combo treble_arm64_aoN-userdebug
+add_lunch_combo treble_arm64_aoS-userdebug
+add_lunch_combo treble_arm64_avN-userdebug
+add_lunch_combo treble_arm64_avS-userdebug
+add_lunch_combo treble_arm64_bfN-userdebug
+add_lunch_combo treble_arm64_bfS-userdebug
+add_lunch_combo treble_arm64_bgN-userdebug
+add_lunch_combo treble_arm64_bgS-userdebug
+add_lunch_combo treble_arm64_boN-userdebug
+add_lunch_combo treble_arm64_boS-userdebug
+add_lunch_combo treble_arm64_bvN-userdebug
+add_lunch_combo treble_arm64_bvS-userdebug
+add_lunch_combo treble_arm_afN-userdebug
+add_lunch_combo treble_arm_afS-userdebug
+add_lunch_combo treble_arm_agN-userdebug
+add_lunch_combo treble_arm_agS-userdebug
+add_lunch_combo treble_arm_aoN-userdebug
+add_lunch_combo treble_arm_aoS-userdebug
+add_lunch_combo treble_arm_avN-userdebug
+add_lunch_combo treble_arm_avS-userdebug
+add_lunch_combo treble_arm_bfN-userdebug
+add_lunch_combo treble_arm_bfS-userdebug
+add_lunch_combo treble_arm_bgN-userdebug
+add_lunch_combo treble_arm_bgS-userdebug
+add_lunch_combo treble_arm_boN-userdebug
+add_lunch_combo treble_arm_boS-userdebug
+add_lunch_combo treble_arm_bvN-userdebug
+add_lunch_combo treble_arm_bvS-userdebug
