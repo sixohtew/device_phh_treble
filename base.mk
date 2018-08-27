9781137e01d65ddee91aa4671619cf5ba48717ff
Pierre-Hugues Husson <phh@phh.me>
2018-08-27 23:23:25 +0200

    Include an XML to describe how to load interfaces java libs

--- base.mk
+++ base.mk
@@ -89,3 +89,5 @@ PRODUCT_PACKAGES += \
 	vendor.qti.hardware.radio.am-V1.0-java \
 	vendor.qti.qcril.am-V1.0-java \
 
+PRODUCT_COPY_FILES += \
+	device/phh/treble/interfaces.xml:system/etc/permissions/interfaces.xml
